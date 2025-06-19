ca00e481520f671e572918fbd58c77ff391fb760
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-06-19 19:57:48 +0100

    Added a separate component for each job listing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Button = ({link, text = 'Button', bg = 'bg-black', bgHover = 'bg-gray-700'}) => {
+const Button = ({link, text = 'Button', bg = 'bg-black', bgHover = 'bg-gray-700', centeredText = false, smallerText = false}) => {
 	return (
 		<a
 			href={link}
-			className={`inline-block ${bg} text-white rounded-lg px-4 py-2 hover:${bgHover}`}
+			className={`inline-block ${bg} text-white rounded-lg px-4 py-2 hover:${bgHover} ${centeredText && 'text-center'} ${smallerText && 'text-sm'}`}
 		>
 			{text}
 		</a>
