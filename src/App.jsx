c5ab730e2dd93d3108c1ad986a6d1c9276474f04
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-06-19 17:10:27 +0100

    Exploring JSX basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div className='text-3xl font-bold underline'>App</div>
-  )
-}
+	const name = "malu";
+	const x = 10;
+	const y = 20;
+	const names = [
+		"Ini",
+		"Pimpy",
+		"Narciso",
+		"Carol",
+		"Cuca",
+		"Matilde",
+		"Martim",
+		"Joana",
+		"Gabi",
+		"JP",
+		"Joao",
+		"Gonçalo",
+		"Iara",
+		"Mafalda",
+		"Leonor",
+	];
+    const loggedIn = true;
+    const styles = {
+        color: 'red',
+        fontSize: '55px'
+    }
 
-export default App
+	return (
+		<>
+			<div className="text-3xl font-bold underline">App</div>
+			<p style={styles}>Oi {name}</p>
+			<p>
+				The sum of {x} and {y} is {x + y}
+			</p>
+			<ul>
+				{names.map((name, index) => (
+					<li key={index}>{name}</li>
+				))}
+			</ul>
+            { loggedIn && <h1>Hello member!</h1>}
+		</>
+	);
+};
+
+export default App;
