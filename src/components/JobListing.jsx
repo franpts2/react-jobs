e05d5a79e87c8accd2512639b22d9c55d08e1ce5
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-06-20 12:22:21 +0100

    Diminished the description in the job listings by default and added a button that makes it bigger or smaller using state

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Button from "./Button";
+import { useState } from "react";
 
 const JobListing = ({ job }) => {
+	const [showFullDescription, setShowFullDescription] = useState(false); //boolean here is the default
+
+	let description = job.description;
+
+	if (!showFullDescription) {
+		description = description.substring(0, 90) + "...";
+	}
+
 	return (
 		<div className="bg-white rounded-xl shadow-md relative">
 			<div className="p-4">
@@ -10,7 +19,14 @@ const JobListing = ({ job }) => {
 					<h3 className="text-xl font-bold">{job.title}</h3>
 				</div>
 
-				<div className="mb-5">{job.description}</div>
+				<div className="mb-5">{description}</div>
+
+				<button
+					onClick={() => setShowFullDescription((prevState) => !prevState)}
+					className="text-indigo-500 mb-5 hover:text-indigo-600"
+				>
+					{showFullDescription ? "Less" : "More"}
+				</button>
 
 				<h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
