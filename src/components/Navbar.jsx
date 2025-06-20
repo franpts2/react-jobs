c72dcde4076b1cb0093a934e8145819c14a03ba0
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-06-20 16:11:30 +0100

    Quick fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 										Jobs
 									</a>
 									<a
-										href="/add-job.html"
+										href="/add-job"
 										className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
 									>
 										Add Job
