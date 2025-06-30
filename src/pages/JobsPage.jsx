dc5143a51e08ca9d8b544bfdb6831c13f01fd3d0
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-06-30 18:23:48 +0100

    Added JobListings to job page and added a parameter to that component to verify if it is in the home page or not

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import JobListings from "../components/JobListings";
 
 const JobsPage = () => {
-  return (
-    <div>JobsPage</div>
-  )
-}
+	return (
+		<section className="bg-blue-60 px-4 py-6">
+			<JobListings />
+		</section>
+	);
+};
 
-export default JobsPage
+export default JobsPage;
