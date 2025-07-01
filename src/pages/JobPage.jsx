c40f7b3574f9d0ff11d7bb2684374d8b9abbf3bb
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-07-01 18:30:56 +0100

    Used useParams from react-router-dom to get id

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const JobPage = () => {
-  return (
-    <div>JobPage</div>
-  )
-}
+	const { id } = useParams();
+	const [job, setJob] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-export default JobPage
+	useEffect(() => {
+		const fetchJob = async () => {
+			try {
+				const res = await fetch(`/api/jobs/${id}`);
+				const data = await res.json();
+				setJob(data);
+			} catch (error) {
+				console.log("Error fetching data", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchJob();
+	}, []);
+
+	return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;
