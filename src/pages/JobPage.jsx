f80a2327df6bdfc2a8aa9ca70c5db3c5d9346d1d
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-07-01 18:54:27 +0100

    Quick fixes to single job page

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,5 @@
 import { useParams, useLoaderData } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const JobPage = () => {
@@ -14,7 +14,7 @@ const JobPage = () => {
 						to="/jobs"
 						className="text-indigo-500 hover:text-indigo-600 flex items-center"
 					>
-						<FaArrowLeft className="mr-2"/> Back to Job Listings
+						<FaArrowLeft className="mr-2" /> Back to Job Listings
 					</Link>
 				</div>
 			</section>
@@ -25,23 +25,19 @@ const JobPage = () => {
 						<main className="md:col-span-2">
 							<div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
 								<div className="text-gray-500 mb-4">{job.type}</div>
-								<h1 className="text-3xl font-bold mb-4">
-									{job.title}
-								</h1>
+								<h1 className="text-3xl font-bold mb-4">{job.title}</h1>
 								<div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-									<i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+									<FaMapMarker className="text-orange-700 mr-1"/>
 									<p className="text-orange-700">{job.location}</p>
 								</div>
 							</div>
 
 							<div className="bg-white p-6 rounded-lg shadow-md mt-6">
-								<h3 className="text-indigo-800 text-lg font-bold mb-6">
+								<h3 className="text-indigo-800 text-lg font-bold mb-4">
 									Job Description
 								</h3>
 
-								<p className="mb-4">
-									{job.description}
-								</p>
+								<p className="mb-4">{job.description}</p>
 
 								<h3 className="text-indigo-800 text-lg font-bold mb-2">
 									Salary
@@ -59,9 +55,7 @@ const JobPage = () => {
 
 								<h2 className="text-2xl">{job.company.name}</h2>
 
-								<p className="my-2">
-									{job.company.description}
-								</p>
+								<p className="my-2">{job.company.description}</p>
 
 								<hr className="my-4" />
 
@@ -73,18 +67,20 @@ const JobPage = () => {
 
 								<h3 className="text-xl">Contact Phone:</h3>
 
-								<p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
+								<p className="my-2 bg-indigo-100 p-2 font-bold">
+									{job.company.contactPhone}
+								</p>
 							</div>
 
 							{/*<!-- Manage -->*/}
 							<div className="bg-white p-6 rounded-lg shadow-md mt-6">
 								<h3 className="text-xl font-bold mb-6">Manage Job</h3>
-								<a
-									href="/add-job.html"
+								<Link
+									to={`/jobs/edit/${job.id}`}
 									className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
 								>
 									Edit Job
-								</a>
+								</Link>
 								<button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
 									Delete Job
 								</button>
